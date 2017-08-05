db699852b356eca6de24d801d586a1ae07985beb
zencree <zencree@hotmail.com>
2017-08-05 17:59:03 -0400

    k

--- snakescript.js
+++ snakescript.js
@@ -1,4 +1,89 @@
 $(document).ready(function() {
-                 alert(1); 
-                  });                  
-                  
+   //Define Vars
+  var canvas =$('#canvas')[0];
+  var ctx = canvas.getContext("2d");
+  var w = canvas.width();
+  var h = canvas.height();
+  var cw = 15;
+  var d = "right";
+  var food;
+  var score;
+  var speed = 130;
+  
+  //Snake Array
+  var snake_array;
+  
+  //Initializer
+  function init() {
+    create_snake();
+    create_food();
+    score = 0;
+    
+    
+    if(typeof game_loop != "undefined") clearInterval(game_loop);
+    game_loop = setInterval(paint,speed);
+  }
+  
+  init(); //Run Initializer
+  
+  //Create Snake
+  function create_snake(){
+    var length = 5;
+    snake_array = [];
+    for(var i = length-1; i >= 0; i--){
+      snake_array.push({x:i,y:0});
+    }
+  }
+  
+  //Create food
+  function create_food(){
+   food = {
+     x:Math.round(Math.random()*(w-cw)/cw),
+     y:Math.round(Math.ramdom()*(h-cw)/cw);
+   };
+  }
+  
+  
+  //Paint Snake
+  function paint(){
+    //Paint The Canvas
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,w,h);
+    ctx.strokeStyle = "white";
+    ctx.strokeRect(0,0,w,h);
+    
+    var nx = snake_array[0].x;
+    var ny = snake_array[0].y;
+    
+    if(d == 'right') nx++;
+    else if(d == 'left') nx--;
+    else if(d == 'up') ny--;
+    else if(d == 'down') ny++;
+  
+   //Collide code 
+  if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(ns, ny, snake_array)) {
+    init();
+    
+    return;
+  }
+    if(nx == food.x && ny == food.y){
+      var tail = {x: nx, y: ny};
+      score++;
+      //Create Food
+      create_food();
+    } else {
+      var tail = snake_array.pop();
+      tail.x = nx; tail.y = ny;
+    }
+     snake_array.unshift(tail);
+    
+    for(var i = 0; i< snake_array.length; i++){
+      var c = snake_array[i];
+      paint_cell(c.x,c.y);
+    }
+    
+    paint_cell(food.x,food.y);
+       //Check Score
+      checkscore(score);
+  }                  
+});
