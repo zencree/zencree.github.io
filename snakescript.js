51eefd7dbf48ed2806f20e72a7ada7d330632c3c
zencree <zencree@hotmail.com>
2017-08-05 21:47:16 -0400

    k

--- snakescript.js
+++ snakescript.js
@@ -102,5 +102,12 @@ $(document).ready(function() {
       }
       return false;
    }
+    //Keyboard Controller
+   $(document).keydown(function(e));
+   var key = e.which;    
+   if(key == "37" && d != "right") d = "left";
+   else if(key == "38" && d!= "down") d ="up";
+   else if(key == "39" && d!= "left") d ="right";
+   else if(key == "40" && d!= "up") d ="down";
   
 });
